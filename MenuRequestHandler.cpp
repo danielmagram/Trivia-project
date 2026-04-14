c1d09232b393240171768c99319e734fc143eb09
root <root@picoctf>
2026-04-14 17:27:17 +0300

    added empty MenuRequestHandler

---
+++ MenuRequestHandler.cpp
@@ -0,0 +1,11 @@
+#include "MenuRequestHandler.h"
+
+bool MenuRequestHandler::isRequestRelevant(RequestInfo info)
+{
+    return false;
+}
+
+RequestResult MenuRequestHandler::handleRequest(RequestInfo info)
+{
+    return RequestResult();
+}
---
+++ MenuRequestHandler.h
@@ -0,0 +1,13 @@
+#pragma once
+#pragma once
+#include "IRequestHandler.h"
+
+class MenuRequestHandler : public IRequestHandler
+{
+public:
+    MenuRequestHandler() = default;
+    ~MenuRequestHandler() = default;
+
+    bool isRequestRelevant(RequestInfo info) override;
+    RequestResult handleRequest(RequestInfo info) override;
+};
