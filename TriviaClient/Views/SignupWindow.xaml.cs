d5374a56d973fbdf7656c330daf808cc3dd7b7b8
oshgru <159oshri>
2026-05-04 17:38:55 +0300

    made startup window and basic signup

---
+++ TriviaClient/Views/SignupWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TriviaClient.Views
+{
+    public partial class SignupWindow : Window
+    {
+        public SignupWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            StartupWindow start = new StartupWindow();
+            start.Show();
+            this.Close();
+        }
+
+        private void Send_Click(object sender, RoutedEventArgs e)
+        {
+            string username = UsernameBox.Text;
+            string password = PasswordBox.Password;
+            string email = EmailBox.Text;
+
+        }
+    }
+}
---
+++ TriviaClient/Views/StartupWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TriviaClient.Views
+{
+    public partial class StartupWindow : Window
+    {
+        public StartupWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SignIn_Click(object sender, RoutedEventArgs e)
+        {
+            LoginWindow login = new LoginWindow();
+            login.Show();
+
+            this.Close(); 
+        }
+
+        private void SignUp_Click(object sender, RoutedEventArgs e)
+        {
+            SignupWindow signup = new SignupWindow();
+            signup.Show();
+
+            this.Close();
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
