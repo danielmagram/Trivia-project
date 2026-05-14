db5e4bf9209e3275e15e6ad22a0a4cc6c4520952
oshgru <159oshri>
2026-05-14 11:45:16 +0300

    room lobby window made

--- TriviaClient/Views/RoomLobbyWindow.xaml.cs
+++ TriviaClient/Views/RoomLobbyWindow.xaml.cs
@@ -11,6 +11,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using TriviaClient.Networking;
+using TriviaClient.Models;
+using TriviaClient.State;
 
 namespace TriviaClient.Views
 {
@@ -19,9 +22,44 @@ namespace TriviaClient.Views
     /// </summary>
     public partial class RoomLobbyWindow : Window
     {
-        public RoomLobbyWindow(int roomId)
+        public RoomLobbyWindow()
         {
             InitializeComponent();
+            UpdateRoom( (uint)SessionData.RoomId);
+        }
+        private void UpdateRoom(uint roomId)
+        {
+            try
+            {
+                Communicator.Instance.SendRequest(105, Serializer.Serialize(new GetPlayersInRoomRequest { roomId = roomId }));
+
+                ResponseInfo info = Communicator.Instance.ReceiveResponse();
+                var response = Serializer.Deserialize<GetPlayersInRoomResponse>(info.JsonPayload);
+
+                if(response.Players.Count != 0)
+                {
+                    ListPlayers.ItemsSource = response.Players;
+                }
+
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to fetch players in room: {ex.Message}", "Network Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        private void UpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateRoom((uint)SessionData.RoomId);
+            MessageBox.Show("Player list updated!");
+        }
+
+        private void LeaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            JoinRoomWindow window = new JoinRoomWindow();
+            window.Show();
+
+            this.Close();
         }
     }
 }
